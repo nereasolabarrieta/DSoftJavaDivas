29cfda45d923656d8c7dadf626d018658abbb28f
maiderdorron <maider.dorronsoro@opendeusto.es>
2020-03-13 10:14:44 +0100

    login hobetuta

--- DSoftJavaDivas/src/main/java/EasyBooking/LP/clsLogIn.java
+++ DSoftJavaDivas/src/main/java/EasyBooking/LP/clsLogIn.java
@@ -8,12 +8,16 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.JLabel;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
+import javax.swing.SwingConstants;
 
 public class clsLogIn extends JFrame {
 
@@ -47,6 +51,7 @@ public class clsLogIn extends JFrame {
 		setBackground(Color.BLACK);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1300, 740);
+		
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -57,26 +62,44 @@ public class clsLogIn extends JFrame {
 		JButton EntrarConFacebook = new JButton();
 		EntrarConFacebook.setLocation(417, 286);
 		EntrarConFacebook.setSize(445,66);
-		ImageIcon Facebook= new ImageIcon("C:\\workspace\\Java_Divas\\Java_Divas\\src\\main\\java\\Easybooking\\Img\\Facebook.JPG");
+		ImageIcon Facebook = new ImageIcon(clsLogIn.class.getResource("/EasyBooking/Img/Facebook.JPG"));
 		EntrarConFacebook.setIcon(Facebook);
-
 		contentPane.add(EntrarConFacebook);
 		
 		JButton EntrarConGoogle = new JButton();
 		EntrarConGoogle.setLocation(417, 425);
 		EntrarConGoogle.setSize(445,66);
-		ImageIcon Google= new ImageIcon("C:\\workspace\\Java_Divas\\Java_Divas\\src\\main\\java\\Easybooking\\Img\\Google.JPG");
+		ImageIcon Google= new ImageIcon(clsLogIn.class.getResource("/EasyBooking/Img/Google.JPG"));
 		EntrarConGoogle.setIcon(Google);
 
 		contentPane.add(EntrarConGoogle);
 		
-		JLabel lblO = new JLabel("-----------------------------  O -----------------------------");
-		lblO.setBounds(449, 380, 484, 30);
-		contentPane.add(lblO);
+		JPanel pCentro = new JPanel();
+		pCentro.setBackground(Color.WHITE);
+		pCentro.setBounds(309, 179, 654, 420);
+		contentPane.add(pCentro);
+		pCentro.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(309, 158, 654, 441);
-		contentPane.add(panel);
+		JLabel lblO = new JLabel("o");
+		lblO.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblO.setBounds(91, 200, 484, 30);
+		pCentro.add(lblO);
+		lblO.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel pNorte = new JPanel();
+		pNorte.setBackground(new Color(0, 0, 128));
+		pNorte.setBounds(0, 0, 1278, 163);
+		contentPane.add(pNorte);
+		pNorte.setLayout(null);
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setBounds(15, 16, 202, 73);
+		ImageIcon fot = new ImageIcon(clsLogIn.class.getResource("/EasyBooking/Img/logo.png"));
+		Icon icono = new ImageIcon(fot.getImage().getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(), Image.SCALE_DEFAULT));
+		lblLogo.setIcon(icono);
+		pNorte.add(lblLogo);
+		
+		
 		
 	
 
