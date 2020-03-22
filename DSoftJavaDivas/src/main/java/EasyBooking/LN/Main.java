8be1e13cc1914934f96fd18249cdd63323ed9211
marizubia9 <m.zubia@opendeusto.es>
2020-03-22 15:07:17 +0100

    Añadir Main y LN

---
+++ DSoftJavaDivas/src/main/java/EasyBooking/LN/Main.java
@@ -0,0 +1,83 @@
+package EasyBooking.LN;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+        {
+			PersistenceManagerFactory persistentManagerFactory = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+			
+			//Insert data in the DB
+			PersistenceManager persistentManager = persistentManagerFactory.getPersistenceManager();				
+			Transaction transaction = persistentManager.currentTransaction();				
+			
+			try
+            {
+			    transaction.begin();
+			    
+			    //Escribir código BD
+			    
+			    transaction.commit();
+			    
+			}
+
+            catch(Exception ex)
+			{
+				System.err.println("* Exception inserting data into db: " + ex.getMessage());
+			}
+			
+			finally
+			{		    
+				if (transaction.isActive()) 
+				{
+			        transaction.rollback();
+			    }
+			    
+			    persistentManager.close();
+			}
+			
+			
+			//Select data using a Query
+			persistentManager = persistentManagerFactory.getPersistenceManager();
+			transaction = persistentManager.currentTransaction();
+				
+			try
+            {
+			    transaction.begin();
+	
+			    //Escribir código BD
+	
+			    transaction.commit();
+			}
+
+			catch(Exception ex)
+			{
+				System.err.println("* Exception executing a query: " + ex.getMessage());
+			}
+
+			finally 
+			{
+				if (transaction.isActive())
+				{
+			        transaction.rollback();
+			    }
+	
+			    persistentManager.close();
+			}
+		}
+
+		catch (Exception ex)
+        {
+			System.err.println("* Exception: " + ex.getMessage());
+		}
+	}
+
+}
