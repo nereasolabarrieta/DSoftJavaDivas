9829ce33574c538d819186e7485e17b86677932c
maiderdorron <maider.dorronsoro@opendeusto.es>
2020-03-28 18:50:50 +0100

    gestor klasea sortu

---
+++ DSoftJavaDivas/src/main/java/EasyBooking/LN/Gestor.java
@@ -0,0 +1,18 @@
+package EasyBooking.LN;
+
+import java.util.ArrayList;
+
+import EasyBooking.LD.Vuelo;
+
+public class Gestor {
+
+	ArrayList<Vuelo>Lista_vuelos;
+	
+	
+	
+	
+	public static void crearVuelo()
+	{
+		
+	}
+}
