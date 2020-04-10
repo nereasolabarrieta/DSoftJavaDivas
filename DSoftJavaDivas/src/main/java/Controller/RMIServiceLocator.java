9a0dde1620d61a25a4c0f410e3ede65de357e238
nereasolabarrieta <nerea.solabarrieta@opendeusto.es>
2020-04-10 15:31:23 +0200

    Base de datosan controller

--- DSoftJavaDivas/src/main/java/Controller/RMIServiceLocator.java
+++ DSoftJavaDivas/src/main/java/Controller/RMIServiceLocator.java
@@ -1,5 +1,17 @@
 package Controller;
 
 public class RMIServiceLocator {
+	private BDController service = null;
 
+    public RMIServiceLocator() { }
+
+    public void setService(String ip, String port, String serviceName) 
+    {    
+ 
+    }
+    
+    public BDController getService() 
+    {
+		return service;    	
+    }
 }
