ac0bf32bd6e3c677984e6162611e2c6aacb8ace0
nereasolabarrieta <nerea.solabarrieta@opendeusto.es>
2020-05-20 18:11:32 +0200

    Buildan beste aldaketa batzuk

--- DSoftJavaDivas/src/main/java/Fachada/ServidorPrincipal.java
+++ DSoftJavaDivas/src/main/java/Fachada/ServidorPrincipal.java
@@ -33,7 +33,7 @@ public class ServidorPrincipal extends UnicastRemoteObject implements itfFachada
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
 
 		String ip = "127.0.0.1";
--- DSoftJavaDivas/src/main/java/Gateway/Gateway.java
+++ DSoftJavaDivas/src/main/java/Gateway/Gateway.java
@@ -14,47 +14,47 @@ public class Gateway {
 	}
 	public void crearConexion()
 	{
-		RestClient_Unmarshalling c1 = new RestClient_Unmarshalling(hostname, port_auth);
-
-		try {
-			c1.makeGetRequest("/");
-			c1.makeGetRequest("/Authentication/Log_in");
-			c1.makeGetRequest("/Authentication/Create_user");
-			c1.makeGetRequest("/Authentication/Change_password");
-			c1.makeGetRequest("/Authentication/Delete_user");
-
-			// real requests
-
-			// todo c1.makePostRequest("This is a client super complex message to send to server");
-		} catch (Exception e) {
-			System.out.println("Catched exception: " + e.getMessage());
-		}
-
-
-		RestClient_Unmarshalling c2 = new RestClient_Unmarshalling(hostname, port_pay);
-
-		try {
-			c2.makeGetRequest("/");
-			c2.makeGetRequest("/Payments/Make_payment");
-			c2.makeGetRequest("/Payments/Create_user");
-			c2.makeGetRequest("/Payments/Update_currency");
-
-			// todo c2.makePostRequest("This is a client super complex message to send to server");
-		} catch (Exception e) {
-			System.out.println("Catched exception: " + e.getMessage());
-		}
-
-		RestClient_Unmarshalling c3 = new RestClient_Unmarshalling(hostname, port_airlines);
-
-		try {
-			c3.makeGetRequest("/");
-			c3.makeGetRequest("/Airlines/search_flights");
-			
-
-			// todo c1.makePostRequest("This is a client super complex message to send to server");
-		} catch (Exception e) {
-			System.out.println("Catched exception: " + e.getMessage());
-		}
+//		RestClient_Unmarshalling c1 = new RestClient_Unmarshalling(hostname, port_auth);
+//
+//		try {
+//			c1.makeGetRequest("/");
+//			c1.makeGetRequest("/Authentication/Log_in");
+//			c1.makeGetRequest("/Authentication/Create_user");
+//			c1.makeGetRequest("/Authentication/Change_password");
+//			c1.makeGetRequest("/Authentication/Delete_user");
+//
+//			// real requests
+//
+//			// todo c1.makePostRequest("This is a client super complex message to send to server");
+//		} catch (Exception e) {
+//			System.out.println("Catched exception: " + e.getMessage());
+//		}
+//
+//
+//		RestClient_Unmarshalling c2 = new RestClient_Unmarshalling(hostname, port_pay);
+//
+//		try {
+//			c2.makeGetRequest("/");
+//			c2.makeGetRequest("/Payments/Make_payment");
+//			c2.makeGetRequest("/Payments/Create_user");
+//			c2.makeGetRequest("/Payments/Update_currency");
+//
+//			// todo c2.makePostRequest("This is a client super complex message to send to server");
+//		} catch (Exception e) {
+//			System.out.println("Catched exception: " + e.getMessage());
+//		}
+//
+//		RestClient_Unmarshalling c3 = new RestClient_Unmarshalling(hostname, port_airlines);
+//
+//		try {
+//			c3.makeGetRequest("/");
+//			c3.makeGetRequest("/Airlines/search_flights");
+//			
+//
+//			// todo c1.makePostRequest("This is a client super complex message to send to server");
+//		} catch (Exception e) {
+//			System.out.println("Catched exception: " + e.getMessage());
+//		}
 	}
 	public void cerrarConexion()
 	{
