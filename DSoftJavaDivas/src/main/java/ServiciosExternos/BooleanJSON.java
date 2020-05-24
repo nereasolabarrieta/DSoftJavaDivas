6564f592e7df355aad0f6bf9001fae45e748f02f
maiderdorron <maider.dorronsoro@opendeusto.es>
2020-05-24 14:05:51 +0200

    prueba

---
+++ DSoftJavaDivas/src/main/java/ServiciosExternos/BooleanJSON.java
@@ -0,0 +1,53 @@
+package ServiciosExternos;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import scala.collection.mutable.SynchronizedSet;
+
+public class BooleanJSON implements Print{
+
+	
+	
+
+
+
+	private String content;
+    private JSONParser parser;
+
+    public BooleanJSON(String content) throws ParseException {
+System.out.println("llego al constr");
+        // We need to parse the input as data is coming from python
+        this.parser = new JSONParser();
+        System.out.println("1");
+        JSONObject json = (JSONObject) parser.parse(content);
+        System.out.println("2");
+        this.content = (String) json.get("Result");
+        System.out.println("3");
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setResult(String content)
+    {
+        this.content = content;
+    }
+
+    public boolean getContentBoolean()
+    {
+    	System.out.println("LO QUE VOY A PARSEAS VA A SER ESTO ");
+    	boolean resp= Boolean.parseBoolean(content);
+        return resp;
+    }
+
+   
+    @Override
+    public void print() {
+        System.out.println("Content of Simple_pass_result class is: " + this.content);
+    }
+
+}
